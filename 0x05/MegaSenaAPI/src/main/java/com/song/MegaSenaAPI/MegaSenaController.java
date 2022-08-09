71f05b8a60bd806410a43d4aa441c76ae22ba724
Joao Bohana <jbohana@ciandt.com>
2022-08-09 14:30:03 -0300

    changed name of method to mensagemBoasVindas

--- 0x05/MegaSenaAPI/src/main/java/com/song/MegaSenaAPI/MegaSenaController.java
+++ 0x05/MegaSenaAPI/src/main/java/com/song/MegaSenaAPI/MegaSenaController.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class MegaSenaController {
 
     @GetMapping("/simpleMessageWelcome")
-    public String boasVindas(){
+    public String mensagemBoasVindas(){
         return "Seja bem vindo!";
     }
 
